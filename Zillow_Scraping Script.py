8f4c824c16cf0065b3af68dc2591e51077e4000a
artbr0wn <art.codingdepot@gmail.com>
2023-08-09 00:13:04 -0400

    Update Zillow_Scraping Script.py
    Simple edits for readability

--- Zillow_Scraping Script.py
+++ Zillow_Scraping Script.py
@@ -10,15 +10,29 @@ from bs4 import BeautifulSoup
 
 # --------------------------------------------------- WEB LINKS ----------------------------------------------------- #
 
-ZILLOW_LINK = "https://www.zillow.com/brooklyn-new-york-ny/rentals/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22north%22%3A40.739135%2C%22east%22%3A-73.833552%2C%22south%22%3A40.570842%2C%22west%22%3A-74.041878%7D%2C%22mapZoom%22%3A12%2C%22isMapVisible%22%3Afalse%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A37607%2C%22regionType%22%3A17%7D%5D%7D"
+ZILLOW_LINK = "https://www.zillow.com/brooklyn-new-york-ny/rentals/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C" \
+              "%22mapBounds%22%3A%7B%22north%22%3A40.739135%2C%22east%22%3A-73.833552%2C%22south%22%3A40.570842%2C" \
+              "%22west%22%3A-74.041878%7D%2C%22mapZoom%22%3A12%2C%22isMapVisible%22%3Afalse%2C%22filterState%22%3A%7B" \
+              "%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value" \
+              "%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22" \
+              "%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22" \
+              "%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C" \
+              "%22isListVisible%22%3Atrue%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A37607%2C%22regionType%22" \
+              "%3A17%7D%5D%7D"
+
 GOOGLE_FORM_LINK = "https://forms.gle/oV4fQHadtj4kSXDd6"
-GOOGLE_FORM_RESPONSE = "https://docs.google.com/forms/d/e/1FAIpQLSdQlyYBnXqLYCQReMNrQ6Mzp7L1xAWkW9ymZ4oixS5lx7AY1A/formResponse"
+
+GOOGLE_FORM_RESPONSE = "https://docs.google.com/forms/d/e/" \
+                       "1FAIpQLSdQlyYBnXqLYCQReMNrQ6Mzp7L1xAWkW9ymZ4oixS5lx7AY1A/formResponse"
+
+# Xpaths variables for locations
 
 ADDRESS_XPATH = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input'
 PRICE_XPATH = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input'
 URL_XPATH = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input'
 SUBMIT_XPATH = '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span'
 
+# Headers - needed for Zillow specifically
 HEADERS = {
     'User-Agent': 'Mozilla/5.0'
 }
@@ -29,17 +43,22 @@ HEADERS = {
 RESPONSE = requests.get(ZILLOW_LINK, headers=HEADERS)
 HTML = RESPONSE.text
 
-# all information for running selenium, ignoring exceptions as they have come up sometimes when locating an element
+# All information for running selenium
+# ignoring exceptions as they have come up sometimes when locating an element
 IGNORED_EXCEPTIONS = (NoSuchElementException, StaleElementReferenceException,)
-OPTIONS = webdriver.ChromeOptions()
+
 # selenium options in case I want to see the webpage remain open after it finishes its run through
+OPTIONS = webdriver.ChromeOptions()
 OPTIONS.add_experimental_option("detach", True)
+
+# selenium webdriver
 driver = webdriver.Chrome()
 driver.get(GOOGLE_FORM_LINK)
 
 
 # lines 37-68 used functions as it was more visible -- decided to future-proof script for other information if necessary
 # these functions were to grab all necessary data from HTML link (addy,url,price) & return respective list w/ data
+
 def grab_address_data(website_html):
 
     soup = BeautifulSoup(website_html, 'html.parser')
@@ -76,9 +95,9 @@ def grab_price_data(website_html):
 
 # lines 72-142 used functions as it was more visible
 # these functions were to use selenium to check if fields were available and return T/F if not,
-# These were used specifically b/c I kept running into a problem where each find_element function wouldn't allow for DOM
-# to load -- so now each function runs a wait.until func that allows for the DOM to load first
-# and I can pinpoint which function is not working via timeout with exception handling.
+# These were used specifically b/c I kept running into a problem where each find_element function wouldn't allow for --
+# DOM to load -- so now each function runs a wait.until func that allows for the DOM to load first
+# I can pinpoint which function is not working via timeout with exception handling.
 
 def find_address():
 
